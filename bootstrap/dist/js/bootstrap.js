54c7c248d781f02e64c9a12807ceb9e8613d4325
Susanta Chakraborty <29665000+susanta96@users.noreply.github.com>
2017-09-19 17:49:43 +0530

    Update bootstrap.js

--- bootstrap/dist/js/bootstrap.js
+++ bootstrap/dist/js/bootstrap.js
@@ -16,15 +16,13 @@ if (typeof jQuery === 'undefined') {
  * Bootstrap: transition.js v3.3.6
  * http://getbootstrap.com/javascript/#transitions
  * ========================================================================
- * Copyright 2011-2015 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
+ */
 
 
 +function ($) {
   'use strict';
 
-  // CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)
+
   // ============================================================
 
   function transitionEnd() {
